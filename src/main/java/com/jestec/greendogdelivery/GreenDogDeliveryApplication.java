1012d6e9df67be46c054c414c4e311d2f2b139ac
Joel Santos <joeleduardodossantos611@.com>
2024-05-01 16:45:34 +0100

    projeto criado

---
+++ src/main/java/com/jestec/greendogdelivery/GreenDogDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.jestec.greendogdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenDogDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenDogDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jestec/greendogdelivery/GreenDogDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jestec.greendogdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenDogDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
